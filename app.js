19d0cdd2a0e838682c98d27476b0bb1afefbcfb8
Eyal Weiss <weiss.e@gmail.com>
2012-11-30 02:31:43 +0200

    Code rearrangement

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , fs = require('fs');
+	, http = require('http')
+	, path = require('path')
+	, fs = require('fs');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
 	app.use(express.favicon());
@@ -22,19 +22,19 @@ app.configure(function(){
 	app.use(express.static(path.join(__dirname, 'assets')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function () {
+	app.use(express.errorHandler());
 });
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.render('index', { title: 'Express' });
 });
 
-app.get('/config-examples/:filename', function(req, res){
+app.get('/config-examples/:filename', function (req, res) {
 	var filename = req.params.filename;
-	fs.readFile('./assets/config-examples/' + filename, function(err, data) {
-		if(err) {
+	fs.readFile('./assets/config-examples/' + filename, function (err, data) {
+		if (err) {
 			res.send(500);
 		} else {
 			res.send(data.toString());
@@ -43,36 +43,40 @@ app.get('/config-examples/:filename', function(req, res){
 });
 
 
-var srcFiles = [
-	'./assets/js/formsEngine/src/header.js',
-	'./assets/js/formsEngine/src/validation.js',
-	'./assets/js/formsEngine/src/namespace.js',
-	'./assets/js/formsEngine/src/fieldTypes.js',
-	'./assets/js/formsEngine/src/core.js'
-];
-var appendFile = function(files, concatSrc, callback){
-	var fname = files.shift();
-	if (fname) {
-		fs.readFile(fname, function(err, data) {
-			if(err) {
-				console.log('err');
-			} else {
-				concatSrc.push(data.toString());
-				appendFile(files, concatSrc, callback);
-			}
-		});
-	} else {
-		callback && callback(concatSrc);
-	}
-}
-var concatSource = ['(function(){'];
-appendFile(srcFiles, concatSource, function(src){
-	src.push('})();');
-	var text = src.join("\n");
-	fs.writeFile('./assets/js/formsEngine/build/formsEngine.js', text);
-});
+(function () {
+	// build script
+	var srcFiles = [
+		'./assets/js/formsEngine/src/header.js',
+		'./assets/js/formsEngine/src/validation.js',
+		'./assets/js/formsEngine/src/namespace.js',
+		'./assets/js/formsEngine/src/fieldTypes.js',
+		'./assets/js/formsEngine/src/core.js'
+	];
+	var appendFile = function (files, concatSrc, callback) {
+		var fname = files.shift();
+		if (fname) {
+			fs.readFile(fname, function (err, data) {
+				if (err) {
+					console.log('err');
+				} else {
+					concatSrc.push(data.toString());
+					appendFile(files, concatSrc, callback);
+				}
+			});
+		} else {
+			callback && callback(concatSrc);
+		}
+	};
+	var concatenated = ['(function(){'];
+	appendFile(srcFiles, concatenated, function (src) {
+		src.push('})();');
+		var text = src.join("\n");
+		fs.writeFile('./assets/js/formsEngine/build/formsEngine.js', text);
+		console.log('Successfully built formsEngine.js');
+	});
+})();
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
 });
